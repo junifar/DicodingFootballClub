25a0b48ab6a79869710c078c847fd5969ee01d2a
junifar <junifar@gmail.com>
2018-10-17 10:04:55 +0700

    Cleaning Final Submission 1

--- app/src/main/java/com/rubahapi/footballclub/FootballClubDetail.kt
+++ app/src/main/java/com/rubahapi/footballclub/FootballClubDetail.kt
@@ -1,19 +1,14 @@
 package com.rubahapi.footballclub
 
-import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import com.rubahapi.footballclub.view.FootballClubDetailUI
-import org.jetbrains.anko.excludeFromRecents
 import org.jetbrains.anko.setContentView
 
 class FootballClubDetail : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        val intent = intent
-//        val bundle = intent.extras
-//        setContentView(R.layout.activity_football_club_detail)
         val image_src = intent.extras?.getInt("items")
         val description = intent.extras?.getString("description")
         FootballClubDetailUI(image_src, description).setContentView(this)
