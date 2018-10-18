89b17d370b619b94026ef1089541262569809742
junifar <junifar@gmail.com>
2018-10-18 22:15:33 +0700

    Revisin for submision

--- app/src/main/java/com/rubahapi/footballclub/FootballClubDetail.kt
+++ app/src/main/java/com/rubahapi/footballclub/FootballClubDetail.kt
@@ -2,8 +2,12 @@ package com.rubahapi.footballclub
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.ImageView
+import android.widget.TextView
 import com.rubahapi.footballclub.view.FootballClubDetailUI
+import com.squareup.picasso.Picasso
 import org.jetbrains.anko.setContentView
+import org.jetbrains.anko.find
 
 class FootballClubDetail : AppCompatActivity() {
 
@@ -12,5 +16,11 @@ class FootballClubDetail : AppCompatActivity() {
         val item = intent.getParcelableExtra<Item>("item")
 
         FootballClubDetailUI(item).setContentView(this)
+
+        val detailImage = find<ImageView>(R.id.detail_image)
+        val detailDescription = find<TextView>(R.id.detail_description)
+
+        item.image?.let { Picasso.get().load(it).fit().into(detailImage) }
+        detailDescription.text = item.description
     }
 }
--- app/src/main/java/com/rubahapi/footballclub/view/FootballClubDetailUI.kt
+++ app/src/main/java/com/rubahapi/footballclub/view/FootballClubDetailUI.kt
@@ -4,22 +4,26 @@ import android.view.Gravity
 import android.view.View
 import com.rubahapi.footballclub.FootballClubDetail
 import com.rubahapi.footballclub.Item
+import com.rubahapi.footballclub.R
 import org.jetbrains.anko.*
 
-class FootballClubDetailUI(private val item:Item): AnkoComponent<FootballClubDetail>{
+class FootballClubDetailUI(private val item: Item): AnkoComponent<FootballClubDetail>{
     override fun createView(ui: AnkoContext<FootballClubDetail>): View = with(ui) {
         return verticalLayout {
             padding = dip(16)
 
             imageView {
-                item.image?.let { setImageResource(it) }
+                id = R.id.detail_image
+//                item.image?.let { setImageResource(it) }
+//                image_src?.let { setImageResource(it) }
             }.lparams(width= dip(200), height = dip(200))
             {
                 gravity = Gravity.CENTER_HORIZONTAL
             }
 
             textView {
-                text = item.description
+                id = R.id.detail_description
+//                text = item.description
             }
 
         }
